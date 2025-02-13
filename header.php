c0875247f69d4c250d5b8697ef047effcaee2854
Axel <schoolandaxel61@gmail.com>
2025-02-13 11:56:53 +0100

    Ajout logo redirection sur index.php

--- header.php
+++ header.php
@@ -63,6 +63,9 @@
     ?>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div>
+        <a href="index.php"><img src="assets/wazaa_logo.png" alt=""></a>
+    </div>
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item">
